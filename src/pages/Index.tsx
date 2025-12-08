a4faa691972d24f783a434ffc925c3aff774f9c0
rajeshvcwork-dev <rajeshvc.work@gmail.com>
2025-12-08 17:21:39 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -70,7 +70,6 @@ const Index = () => {
                 </Button>
               </Link>
               <Link to="/portfolio" className="px-8 py-4 rounded-full border-2 border-background/30 text-background font-medium hover:bg-background/10 transition-all">
-                <Button variant="outline" size="xl">
                   View Portfolio
                 </Button>
               </Link>
