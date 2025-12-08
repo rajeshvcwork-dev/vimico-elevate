1153b7ed39679e1e067cb795eaa9acd8c2dbb467
rajeshvcwork-dev <rajeshvc.work@gmail.com>
2025-12-08 17:31:30 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -70,8 +70,8 @@ const Index = () => {
                 </Button>
               </Link>
               <Link to="/portfolio">
-                <Button variant="outline" size="xl">   
-                  View Portfolio
+                <Button variant="outline" size="xl" className="gap-2">   
+                  View Portfolio <ArrowRight className="w-5 h-5" />
                 </Button>  
               </Link>
             </div>
