22fa5553533d2e2cef3ab742b223f8b64b6193d7
rajeshvc.workspace <rajeshvc.work@gmail.com>
2026-07-09 21:09:10 +0530

    Bold additional features text in Resellers page
    Bold the text 'And Many more features...' in the features list.

--- src/pages/Resellers.tsx
+++ src/pages/Resellers.tsx
@@ -28,7 +28,7 @@ const resellerProducts = [
       "GPS location tracking",
       "Social media monitoring",
       "Remote camera and microphone access",
-      "And Many more features...",
+      <b>"And Many more features..."</b>,
     ],
     link: "https://www.flexispy.com?a_aid=e464f5c3", // Placeholder for reseller link
     icon: Shield,
